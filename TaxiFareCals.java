3f037018398b0850fbf62b8a8aac852684ead9c9
nt.hieuu <133157197+l3w1sh3r@users.noreply.github.com>
2024-10-01 01:22:30 +0700

    Update and rename hw1.java to TaxiFareCals.java

---
+++ TaxiFareCals.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class TaxiFareCals {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double km = scanner.nextDouble();
+        double totalFare = 0;
+        if (km < 0) {
+            System.out.print("Invalid input\n");
+        } else if (km <= 1) {
+            totalFare = 15000;
+        } else if (km <= 5) {
+            totalFare = 15000 + (km - 1) * 13000;
+        } else {
+            totalFare = 15000 + 4 * 13000 + (km - 5) * 11000;
+        }
+        if (km > 120) {
+            totalFare = totalFare * 0.9;
+        }
+
+        System.out.printf("Total Fare: %.2f VND\n", totalFare);
+
+        scanner.close();
+    }
+}
+
